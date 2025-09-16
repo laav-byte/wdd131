c80c3c99adaeffffda3b6a9c539bec613a13ddd4
laav-byte <lvilla1@byupathway.edu>
2025-09-16 22:58:23 +0800

    bom actvity added

---
+++ Week02/scripts/bom.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#favorite');
+const button = document.querySelector('button');
+const list = document.querySelector();
+const li= document.createElement('li');
+const deleteBtn = document.createElement('button');
+
+li.textContent = input.value;
+deleteBtn.textContent = 'Delete';
+li.appendChild(deleteBtn);
+list.appendChild(li);
