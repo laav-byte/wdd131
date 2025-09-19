85d8b7806320a2558367e520843ce1ad4c765c57
laav-byte <lvilla1@byupathway.edu>
2025-09-19 09:22:03 +0800

    js updated for bom activity

--- Week02/scripts/bom.js
+++ Week02/scripts/bom.js
@@ -1,10 +1,28 @@
-const input = document.querySelector('#favorite');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector();
-const li= document.createElement('li');
-const deleteBtn = document.createElement('button');
+const list = document.querySelector('#list');
 
-li.textContent = input.value;
-deleteBtn.textContent = 'Delete';
-li.appendChild(deleteBtn);
-list.appendChild(li);
+
+button.addEventListener('click', function() {
+
+    const li= document.createElement('li');
+    const deleteBtn = document.createElement('button');
+
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteBtn.textContent = 'Delete';
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+
+        deleteBtn.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+          });
+
+        input.value = '';
+        input.focus();
+    }
+});
+
+
+
